9ed2327cdab82a57925947a00fd45ca7ef064204
jardel88 <jardelsilvagomes@hotmail.com>
2022-06-09 16:51:02 -0300

    Demais operacoes aritmeticas

--- src/main/kotlin/br/com/erudio/MathController.kt
+++ src/main/kotlin/br/com/erudio/MathController.kt
@@ -25,8 +25,52 @@ class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo)
     }
 
+    @RequestMapping(value = ["/subtraction/{numberOne}/{numberTwo}"])
+    fun subtraction(@PathVariable(value = "numberOne") numberOne: String?,
+                    @PathVariable(value = "numberTwo") numberTwo: String?
+    ): Double{
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw UnsuppertedMathOperationException("Caractere invalido.")
+        return convertToDouble(numberOne) - convertToDouble(numberTwo)
+    }
+
+    @RequestMapping(value = ["/multplication/{numberOne}/{numberTwo}"])
+    fun multplication(@PathVariable(value = "numberOne") numberOne: String?,
+                      @PathVariable(value = "numberTwo") numberTwo: String?
+    ): Double{
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw UnsuppertedMathOperationException("Caractere invalido.")
+        return convertToDouble(numberOne) * convertToDouble(numberTwo)
+    }
+
+    @RequestMapping(value = ["/division/{numberOne}/{numberTwo}"])
+    fun division(@PathVariable(value = "numberOne") numberOne: String?,
+                 @PathVariable(value = "numberTwo") numberTwo: String?
+    ): Double{
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw UnsuppertedMathOperationException("Caractere invalido.")
+        return convertToDouble(numberOne) / convertToDouble(numberTwo)
+    }
+
+    @RequestMapping(value = ["/mean/{numberOne}/{numberTwo}"])
+    fun mean(@PathVariable(value = "numberOne") numberOne: String?,
+             @PathVariable(value = "numberTwo") numberTwo: String?
+    ): Double{
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw UnsuppertedMathOperationException("Caractere invalido.")
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2
+    }
+
+    @RequestMapping(value = ["/squareRoot/{number}"])
+    fun squareRoot(@PathVariable(value = "number") number: String?
+    ): Double{
+        if (!isNumeric(number))
+            throw UnsuppertedMathOperationException("Caractere invalido.")
+        return Math.sqrt(convertToDouble(number))
+    }
+
     private fun convertToDouble(strNumber: String?): Double {
-       if (strNumber.isNullOrEmpty()) return 0.0
+        if (strNumber.isNullOrEmpty()) return 0.0
         //BR 10,20 US 10.20
         //convertendo de BR para US
         val number = strNumber.replace(",".toRegex(), ".")
