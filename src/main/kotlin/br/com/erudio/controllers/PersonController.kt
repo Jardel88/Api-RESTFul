048526973769ed8e364a05bcf837f768dff4b714
jardel88 <jardelsilvagomes@hotmail.com>
2022-06-10 17:00:46 -0300

    Versionamento de end-point P1

--- src/main/kotlin/br/com/erudio/controllers/PersonController.kt
+++ src/main/kotlin/br/com/erudio/controllers/PersonController.kt
@@ -1,20 +1,12 @@
 package br.com.erudio.controllers
 
 import br.com.erudio.data.vo.v1.PersonVO
-import br.com.erudio.model.Person
+import br.com.erudio.data.vo.v2.PersonVO as PersonVOV2
 import br.com.erudio.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/person")
@@ -45,6 +37,15 @@ class PersonController {
     fun create(@RequestBody person: PersonVO): PersonVO{
         return service.create(person)
     }
+
+    @PostMapping(value = ["/v2"],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun createV2(@RequestBody person: PersonVOV2): PersonVOV2{
+        return service.createV2(person)
+    }
+
 
     @PutMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE],
