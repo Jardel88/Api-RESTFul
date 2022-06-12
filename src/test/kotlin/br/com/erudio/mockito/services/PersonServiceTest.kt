f63cb95f04d9049ca409c4c0d8b2280bd1a478cd
jardel88 <jardelsilvagomes@hotmail.com>
2022-06-12 18:36:54 -0300

    Criacao classe de test

---
+++ src/test/kotlin/br/com/erudio/mockito/services/PersonServiceTest.kt
@@ -0,0 +1,60 @@
+package br.com.erudio.mockito.services
+
+import br.com.erudio.repository.PersonRepository
+import br.com.erudio.services.PersonService
+import br.com.erudio.unittests.mapper.mocks.MockPerson
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+internal class PersonServiceTest {
+
+    private lateinit var inputObject: MockPerson
+
+    @InjectMocks
+    private lateinit var service: PersonService
+
+    @Mock
+    private lateinit var repository: PersonRepository
+
+    @BeforeEach
+    fun setUpMocks() {
+        inputObject = MockPerson()
+        MockitoAnnotations.openMocks(this)
+    }
+
+    @Test
+    fun findAll() {
+    }
+
+    @Test
+    fun findById() {
+    }
+
+    @Test
+    fun create() {
+    }
+
+    @Test
+    fun createV2() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun delete() {
+    }
+}
