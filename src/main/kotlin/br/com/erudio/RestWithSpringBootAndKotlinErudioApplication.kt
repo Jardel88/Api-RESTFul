ac3d6c418d75a79d7652c3d1005eea7507b41b66
jardel88 <jardelsilvagomes@hotmail.com>
2022-06-17 14:57:03 -0300

    Implementacao SecurityConfig

--- src/main/kotlin/br/com/erudio/RestWithSpringBootAndKotlinErudioApplication.kt
+++ src/main/kotlin/br/com/erudio/RestWithSpringBootAndKotlinErudioApplication.kt
@@ -3,9 +3,24 @@ package br.com.erudio
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+// import org.springframework.security.crypto.password.DelegatingPasswordEncoder
+// import org.springframework.security.crypto.password.PasswordEncoder
+// import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
+// import java.util.HashMap
+
 @SpringBootApplication
 class RestWithSpringBootAndKotlinErudioApplication
 
 fun main(args: Array<String>) {
 	runApplication<RestWithSpringBootAndKotlinErudioApplication>(*args)
+
+	/**
+	val encoders: MutableMap<String, PasswordEncoder> = HashMap()
+	encoders["pbkdf2"] = Pbkdf2PasswordEncoder()
+	val passwordEncoder = DelegatingPasswordEncoder("pbkdf2", encoders)
+	passwordEncoder.setDefaultPasswordEncoderForMatches(Pbkdf2PasswordEncoder())
+
+	val result = passwordEncoder.encode("foo-bar")
+	println("My hash $result")
+	 */
 }
